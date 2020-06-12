8089a1ee2341362398cad11c927ec539e8824980
jiangjiwei <jiangjiwei0322@outlook.com>
2020-06-12 17:31:29 +0800

    feat: 创建 Plugin 实现类和指向文件

---
+++ gradlePlugin/src/main/java/me/monster/gradle/plugin/Entrance.java
@@ -0,0 +1,12 @@
+package me.monster.gradle.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class Entrance implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+
+    }
+}
